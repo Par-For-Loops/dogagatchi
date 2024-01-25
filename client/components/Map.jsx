b00b19c19a94600ff8b7d1423139c2d3887b5f53
student <student@Carins-MacBook-Pro.local>
2024-01-25 16:29:35 -0600

    marker rendering/lnglat logging

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -9,17 +9,37 @@ function MyMap() {
     const map = useRef(null);
     const [lng, setLng] = useState(-90.09307721786142);
     const [lat, setLat] = useState(30.008068345656227);
-    const [zoom, setZoom] = useState(14);
+    const [zoom, setZoom] = useState(13);
 
+    // Set marker options.
+    
     useEffect(() => {
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
-          container: mapContainer.current,
-          style: 'mapbox://styles/mapbox/satellite-streets-v11',
-          center: [lng, lat],
-          zoom: zoom
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/satellite-streets-v11',
+            center: [lng, lat],
+            zoom: zoom
         });
-      });
+        createMarker()
+    });
+    
+    
+    function createMarker() {
+        // var coordinates = e.lngLat
+        // console.log(coordinates)
+        map.marker = new mapboxgl.Marker({
+            color: "orange",
+            draggable: false
+        }).setLngLat([lng, lat])
+            .addTo(map.current);
+        const lngLat = map.marker.getLngLat()
+        console.log(lngLat)
+    }
+      
+    function clickForNewMarker(){
+        
+    }
 
       return (
         <div>
